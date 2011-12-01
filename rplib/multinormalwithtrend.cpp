745505249dbe62dfedc20d116e1ee2ad5600539b
fjellvoll <fjellvoll@176de4d3-4747-0410-aa96-47f6d6634214>
2011-12-01 13:42:02 +0000

    Class MultiNormalWithTrend is finished. Added some test sampling functions to class.
    git-svn-id: https://code.nr.no/scm/svn/crava/branches/StatoilDevelopment@1230 176de4d3-4747-0410-aa96-47f6d6634214

--- rplib/multinormalwithtrend.cpp
+++ rplib/multinormalwithtrend.cpp
@@ -6,13 +6,15 @@
 
 #include "lib/lib_matr.h"
 
+#include <cmath>
+
 
 
 
 MultiNormalWithTrend::
-MultiNormalWithTrend(const NRLib::Distribution<double>&        vp, 
-                     const NRLib::Distribution<double>&        vs, 
-                     const NRLib::Distribution<double>&        rho,
+MultiNormalWithTrend(const NRLib::Normal&                      vp, 
+                     const NRLib::Normal&                      vs, 
+                     const NRLib::Normal&                      rho,
                      const Trend&                              mean_trend_vp,
                      const Trend&                              mean_trend_vs,
                      const Trend&                              mean_trend_rho,
@@ -48,14 +50,7 @@ MultiNormalWithTrend::ReSample(double s1, double s2,
   rc[2] = rho_.Draw();
 
   // fill cov matrix
-  double ** cov_matrix = new double*[3];
-  for (int i = 0; i < 3; ++i)
-    cov_matrix[i] = new double[3];
-
-  for (int j = 0; j < 3; ++j) {
-    for (int i = 0; i < 3; ++i)
-      cov_matrix[j][i] = cov_trend_[j][i]->GetValue(s1, s2);
-  }
+  double ** cov_matrix = CreateCovMatrix(s1, s2);
   
   //cholesky 
   lib_matrCholR(3, cov_matrix); 
@@ -66,11 +61,170 @@ MultiNormalWithTrend::ReSample(double s1, double s2,
   vs  =  rhs[1] + mean_trend_vs_.GetValue(s1, s2);
   rho =  rhs[2] + mean_trend_rho_.GetValue(s1, s2);
 
-  //clean up
-  for (int i = 0; i < 3; ++i)
-    delete [] cov_matrix[i];
-  delete [] cov_matrix;
+  DeleteCovMatrix(cov_matrix);
 
+}
+
+void     
+MultiNormalWithTrend::ReSample(double s1, double s2,
+                               double** cov_matrix_cholesky,
+                               double& vp, double& vs, double& rho,
+                               bool is_cholesky) const {
+
+  std::vector<double> rhs(3); // right hand side
+  std::vector<double> rc(3);  // random component
+
+  rc[0] = vp_.Draw();
+  rc[1] = vs_.Draw();
+  rc[2] = rho_.Draw();
+  
+  //cholesky 
+  if (!is_cholesky)
+    lib_matrCholR(3, cov_matrix_cholesky); 
+
+  MatrProdTranspCholVecRR(3, cov_matrix_cholesky, &rc[0] , &rhs[0]); // rhs is given value 
+
+  vp  =  rhs[0] + mean_trend_vp_.GetValue(s1, s2);
+  vs  =  rhs[1] + mean_trend_vs_.GetValue(s1, s2);
+  rho =  rhs[2] + mean_trend_rho_.GetValue(s1, s2);
+
+}
+
+void     
+MultiNormalWithTrend::EstimateExpectation(double s1, double s2, int sample_size, 
+                                          double& exp_vp, double& exp_vs, double& exp_rho) const {
+  
+  exp_vp = exp_vs = exp_rho = 0;
+
+  if (sample_size <= 0)
+    return;
+
+  // fill cov matrix
+  double ** cov_matrix = CreateCovMatrix(s1, s2);
+
+  // cholesky
+  lib_matrCholR(3, cov_matrix);
+
+  for (int i = 0; i < sample_size; ++i) {
+    double vp, vs, rho;
+    ReSample(s1, s2, cov_matrix,
+             vp, vs, rho);
+
+    exp_vp  += vp;
+    exp_vs  += vs;
+    exp_rho += rho;
+
+  }
+
+  exp_vp  /= sample_size;
+  exp_vs  /= sample_size;
+  exp_rho /= sample_size;
+
+  DeleteCovMatrix(cov_matrix);
+
+}
+
+void     
+MultiNormalWithTrend::EstimateExpectationAndVariance(double s1, double s2, int sample_size,
+                                                     double& exp_vp, double& exp_vs, double& exp_rho,
+                                                     double& var_vp, double& var_vs, double& var_rho) const {
+  
+  exp_vp = exp_vs = exp_rho = 0;
+  var_vp = var_vs = var_rho = 0;
+
+  if (sample_size <= 0)
+    return;
+
+  // fill cov matrix
+  double ** cov_matrix = CreateCovMatrix(s1, s2);
+
+  // cholesky
+  lib_matrCholR(3, cov_matrix);
+
+  for (int i = 0; i < sample_size; ++i) {
+    double vp, vs, rho;
+    ReSample(s1, s2, cov_matrix,
+             vp, vs, rho);
+
+    exp_vp  += vp;
+    exp_vs  += vs;
+    exp_rho += rho;
+
+    var_vp  += vp*vp;
+    var_vs  += vs*vs;
+    var_rho += rho*rho;
+
+  }
+
+  exp_vp  /= sample_size;
+  exp_vs  /= sample_size;
+  exp_rho /= sample_size;
+
+  var_vp   = var_vp/sample_size - exp_vp*exp_vp;
+  var_vs   = var_vs/sample_size - exp_vs*exp_vs;
+  var_rho  = var_rho/sample_size - exp_rho*exp_rho;
+
+  DeleteCovMatrix(cov_matrix);
+
+}
+
+double** 
+MultiNormalWithTrend::CreateEstimateOfCovMatrix(double s1, double s2, int sample_size) const {
+  if (sample_size <= 0)
+    return NULL;
+
+  double exp_vp, exp_vs, exp_rho;
+  double var_vp, var_vs, var_rho;
+
+  exp_vp = exp_vs = exp_rho = 0;
+  var_vp = var_vs = var_rho = 0;
+  
+
+  // fill cov matrix
+  double ** cov_matrix = CreateCovMatrix(s1, s2);
+
+  // cholesky
+  lib_matrCholR(3, cov_matrix);
+
+  double vp_vs   = 0;
+  double vp_rho  = 0;
+  double vs_rho  = 0;
+
+  for (int i = 0; i < sample_size; ++i) {
+    double vp = 0, vs = 0, rho = 0;
+    ReSample(s1, s2, cov_matrix,
+             vp, vs, rho);
+
+    vp_vs    += vp*vs;
+    vp_rho   += vp*rho;
+    vs_rho   += vs*rho;
+
+    exp_vp  += vp;
+    exp_vs  += vs;
+    exp_rho += rho;
+
+    var_vp  += vp*vp;
+    var_vs  += vs*vs;
+    var_rho += rho*rho;
+  }
+
+  exp_vp  /= sample_size;
+  exp_vs  /= sample_size;
+  exp_rho /= sample_size;
+
+  var_vp   = var_vp/sample_size - exp_vp*exp_vp;
+  var_vs   = var_vs/sample_size - exp_vs*exp_vs;
+  var_rho  = var_rho/sample_size - exp_rho*exp_rho;
+
+  vp_vs     = vp_vs/sample_size - exp_vp*exp_vs;
+  vp_rho    = vp_rho/sample_size - exp_vp*exp_rho;
+  vs_rho    = vs_rho/sample_size - exp_vs*exp_rho;
+
+  cov_matrix[0][0] = var_vp;  cov_matrix[0][1] = vp_vs;   cov_matrix[0][2] = vp_rho;
+  cov_matrix[1][0] = vp_vs;   cov_matrix[1][1] = var_vs;  cov_matrix[1][2] = vs_rho;
+  cov_matrix[2][0] = vp_rho;  cov_matrix[2][1] = vs_rho;  cov_matrix[2][2] = var_rho; 
+
+  return cov_matrix;
 }
 
 void
@@ -86,5 +240,29 @@ MatrProdTranspCholVecRR(int n, double **mat, double *in_vec,double* out_vec) con
       out_vec[i] += mat[i][j]*in_vec[j]; 
     }
   }
+}
+
+double** 
+MultiNormalWithTrend::CreateCovMatrix(double s1, double s2) const {
+  // fill cov matrix
+  double ** cov_matrix = new double*[3];
+  for (int i = 0; i < 3; ++i)
+    cov_matrix[i] = new double[3];
+
+  for (int j = 0; j < 3; ++j) {
+    for (int i = 0; i < 3; ++i)
+      cov_matrix[j][i] = cov_trend_[j][i]->GetValue(s1, s2);
+  }
+
+  return cov_matrix;
+}
+
+void     
+MultiNormalWithTrend::DeleteCovMatrix(double** cov_matrix) const {
+  //clean up
+  for (int i = 0; i < 3; ++i)
+    delete [] cov_matrix[i];
+  delete [] cov_matrix;
+
 }
 
--- rplib/multinormalwithtrend.h
+++ rplib/multinormalwithtrend.h
@@ -1,7 +1,7 @@
 #ifndef RPLIB_MULTINORMALWITHTREND_H
 #define RPLIB_MULTINORMALWITHTREND_H
 
-#include "nrlib/random/distribution.hpp"
+#include "nrlib/random/normal.hpp"
 
 #include <vector>
 
@@ -9,9 +9,9 @@ class Trend;
 
 class MultiNormalWithTrend {
  public:
-   MultiNormalWithTrend(const NRLib::Distribution<double>&        vp, 
-                        const NRLib::Distribution<double>&        vs, 
-                        const NRLib::Distribution<double>&        rho,
+   MultiNormalWithTrend(const NRLib::Normal&                      vp, 
+                        const NRLib::Normal&                      vs, 
+                        const NRLib::Normal&                      rho,
                         const Trend&                              mean_trend_vp,
                         const Trend&                              mean_trend_vs,
                         const Trend&                              mean_trend_rho,
@@ -21,14 +21,30 @@ class MultiNormalWithTrend {
    
    void     ReSample(double s1, double s2, 
                      double& vp, double& vs, double& rho) const; 
+
+   void     ReSample(double s1, double s2,
+                     double** cov_matrix_cholesky,
+                     double& vp, double& vs, double& rho,
+                     bool is_cholesky = true) const;
+
+   void     EstimateExpectation(double s1, double s2, int sample_size,
+                                double& exp_vp, double& exp_vs, double& exp_rho) const;
+
+   void     EstimateExpectationAndVariance(double s1, double s2, int sample_size,
+                                           double& exp_vp, double& exp_vs, double& exp_rho,
+                                           double& var_vp, double& var_vs, double& var_rho) const;
+
+   double** CreateEstimateOfCovMatrix(double s1, double s2, int sample_size) const;
  private:
-   void MatrProdTranspCholVecRR(int n, double **mat, double *in_vec,double* out_vec) const; //NBNB fjellvoll replace with library function?
+   void     MatrProdTranspCholVecRR(int n, double **mat, double *in_vec,double* out_vec) const; //NBNB fjellvoll replace with library function?
+   double** CreateCovMatrix(double s1, double s2) const;
+   void     DeleteCovMatrix(double** cov_matrix) const;
   
 
  private:
-  const NRLib::Distribution<double>&        vp_;              // standardized distribution of vp 
-  const NRLib::Distribution<double>&        vs_;              // standardized distribution of vs given vp
-  const NRLib::Distribution<double>&        rho_;             // standardized distribution of rho given vp and vs
+  const NRLib::Normal&                      vp_;              // standardized distribution of vp 
+  const NRLib::Normal&                      vs_;              // standardized distribution of vs given vp
+  const NRLib::Normal&                      rho_;             // standardized distribution of rho given vp and vs
   const Trend&                              mean_trend_vp_;   // mean trend of vp
   const Trend&                              mean_trend_vs_;   // mean trend of vs
   const Trend&                              mean_trend_rho_;  // mean trend of rho
