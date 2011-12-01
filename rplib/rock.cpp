daefca9be2f72de83ee886a9f23a68c2b910a910
heidi <heidi@176de4d3-4747-0410-aa96-47f6d6634214>
2011-12-01 13:59:38 +0000

    CRA-236: Forgot initialization of a static in last commit.
    git-svn-id: https://code.nr.no/scm/svn/crava/branches/StatoilDevelopment@1231 176de4d3-4747-0410-aa96-47f6d6634214

--- rplib/rock.cpp
+++ rplib/rock.cpp
@@ -1,5 +1,6 @@
 #include "rplib/rock.h"
 
+std::vector<Fluid *> Rock::fluid_(0);
 
 Rock::Rock(const std::vector<double> & param, const std::vector<double> & saturation) 
 : param_(param), saturation_(saturation)
