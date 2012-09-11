b754459d86d3ded9d83b7ebc55048b66f662bb1a
pdahle <pdahle@176de4d3-4747-0410-aa96-47f6d6634214>
2012-09-11 11:17:12 +0000

    Test of Git update from svn functionality
    git-svn-id: https://code.nr.no/scm/svn/crava/trunk@1447 176de4d3-4747-0410-aa96-47f6d6634214

--- main.cpp
+++ main.cpp
@@ -132,7 +132,7 @@ int main(int argc, char** argv)
 
   Program program( 1,                     // Major version
                    3,                     // Minor version
-                   0,                     // Patch number
+                   0,                     // Patch number for bug fixes
                    //"",                  // Use empty string "" for release versions
                    " beta",               // Use empty string "" for release versions
                    -1,                    // Validity of licence in days (-1 = infinite)
