8badc3c77c74d901fb23394075af36f75006494c
Bjørn Fjellvoll <fjellvoll@nr.no>
2013-09-04 07:45:15 +0200

    CRA-570

--- rplib/surf_rho1.h
+++
@@ -1,8 +0,0 @@
-//This file is generated automatically with the function WriteHeaderFileForStoringSurface located in rplib/fluidco2.cpp.
-#ifndef RPLIB_FLUID_SURF_RHO1_H
-#define RPLIB_FLUID_SURF_RHO1_H
-#include "nrlib/surface/regularsurface.hpp"
-namespace ConstDataStoredAsSurface {
-const NRLib::RegularSurface<double> CreateSurfaceRho1();
-}
-#endif
--- rplib/surf_rho2.h
+++
@@ -1,8 +0,0 @@
-//This file is generated automatically with the function WriteHeaderFileForStoringSurface located in rplib/fluidco2.cpp.
-#ifndef RPLIB_FLUID_SURF_RHO2_H
-#define RPLIB_FLUID_SURF_RHO2_H
-#include "nrlib/surface/regularsurface.hpp"
-namespace ConstDataStoredAsSurface {
-const NRLib::RegularSurface<double> CreateSurfaceRho2();
-}
-#endif
